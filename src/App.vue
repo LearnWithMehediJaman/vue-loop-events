d0fcad127b7033e2842669cda4b9f6287d52fc0d
Mehedi Jaman <mail4mjaman@gmail.com>
2024-01-31 09:08:24 +0600

    - Vue loop - Vue Events - Two Way data binding

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<script setup>
+import Loop from './components/Loop.vue';
+import DataModel from './components/DataModel.vue';
+</script>
+<template>
+  <!-- <Loop></Loop> -->
+  <DataModel></DataModel>
+</template>
---
+++ src/components/Loop.vue
@@ -0,0 +1,58 @@
+<script setup>
+const persons = [
+  {
+    'name': 'John Doe',
+    'age': 30,
+    'city': 'New York'
+  },
+
+  {
+    'name': 'Jimmy Doe',
+    'age': 30,
+    'city': 'New York'
+  },
+
+  {
+    'name': 'Jane Doe',
+    'age': 30,
+    'city': 'New York'
+  },
+
+  {
+    'name': 'Jackie Doe',
+    'age': 30,
+    'city': 'New York'
+  },
+
+  {
+    'name': 'Jenny Doe',
+    'age': 30,
+    'city': 'New York'
+  },
+
+  {
+    'name': 'Jenny Doe',
+    'age': 30,
+    'city': 'New York'
+  }
+];
+
+</script>
+
+<template>
+  <table class="table-auto w-1/2 mt-4 ml-5 bg-blue-300 ">
+    <thead>
+      <th class="border border-slate-500 p-2">Name</th>
+      <th class="border border-slate-500 p-2">Age</th>
+      <th class="border border-slate-500 p-2">Location</th>
+    </thead>
+
+    <tbody>
+      <tr v-for="(person, index) in persons" :key="index">
+        <td class="border border-slate-500 text-center p-2">{{ person.name }}</td>
+        <td class="border border-slate-500 text-center p-2">{{ person.age }}</td>
+        <td class="border border-slate-500 text-center p-2">{{ person.city }}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
